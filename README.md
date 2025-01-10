389a3ef8f11142ed8207b43d485ace6ca14988d6
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2025-01-10 12:02:12 -0500

    output more information about instruction arguments with better formatting
    Added annotations for basic jumps (JUMP and equality operators), loads,
    and procedure calls. I'm not sure how I feel about printing everything
    in hexadecimal, as it makes comparing the disassembly to the source
    `.asm` files more tedious.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ zig run q3disasm.zig -- -help
 ```
 
 ## Example
-Here's the truncated output when running the tool on the assembled [`cgame.qvm`](https://github.com/id-Software/Quake-III-Arena/blob/master/code/cgame/cg_main.c).
+Here's the truncated output when running the tool on an assembled [`cgame.qvm`](https://github.com/id-Software/Quake-III-Arena/blob/master/code/cgame/cg_main.c).
 ```
 $ ./q3disasm -header cgame.qvm
 cgame.qvm header:
@@ -24,144 +24,144 @@ cgame.qvm header:
     lit_length:        20384
     bss_length:        3856588
 
-vmMain:
-    ENTER 36
-    LOCAL 20
-    LOCAL 44
-    LOAD4
-    STORE4
-    LOCAL 20
-    LOAD4
-    CONST 0
-    LTI 123
-    LOCAL 20
-    LOAD4
-    CONST 8
-    GTI 123
-    LOCAL 20
-    LOAD4
-    CONST 2
-    LSH
-    CONST 8
-    ADD
-    LOAD4
-    JUMP
-    LOCAL 48
-    LOAD4
-    ARG 8
-    LOCAL 52
-    LOAD4
-    ARG 12
-    LOCAL 56
-    LOAD4
-    ARG 16
-    CONST 3356
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    CONST 3638
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 24
-    CONST 4060
-    CALL
-    STORE4
-    LOCAL 24
-    LOAD4
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 48
-    LOAD4
-    ARG 8
-    LOCAL 52
-    LOAD4
-    ARG 12
-    LOCAL 56
-    LOAD4
-    ARG 16
-    CONST 65579
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 28
-    CONST 381
-    CALL
-    STORE4
-    LOCAL 28
-    LOAD4
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 32
-    CONST 398
-    CALL
-    STORE4
-    LOCAL 32
-    LOAD4
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 48
-    LOAD4
-    ARG 8
-    LOCAL 52
-    LOAD4
-    ARG 12
-    CONST 3644
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 48
-    LOAD4
-    ARG 8
-    LOCAL 52
-    LOAD4
-    ARG 12
-    CONST 3647
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    LOCAL 48
-    LOAD4
-    ARG 8
-    CONST 3641
-    CALL
-    POP
-    CONST 0
-    LEAVE 36
-    CONST 133
-    JUMP
-    CONST 15849
-    ARG 8
-    LOCAL 44
-    LOAD4
-    ARG 12
-    CONST 440
-    CALL
-    POP
-    CONST -1
-    LEAVE 36
-    PUSH
-    LEAVE 36
+vmMain():
+00000000      ENTER 0x24      
+00000001      LOCAL 0x14      
+00000002      LOCAL 0x2C      
+00000003      LOAD4
+00000004     STORE4
+00000005      LOCAL 0x14      
+00000006      LOAD4
+00000007      CONST 0x0       
+00000008        LTI 0x7B       ; vmMain()+0x7B (0000007B)
+00000009      LOCAL 0x14      
+0000000A      LOAD4
+0000000B      CONST 0x8       
+0000000C        GTI 0x7B       ; vmMain()+0x7B (0000007B)
+0000000D      LOCAL 0x14      
+0000000E      LOAD4
+0000000F      CONST 0x2       
+00000010        LSH
+00000011      CONST 0x8       
+00000012        ADD
+00000013      LOAD4
+00000014       JUMP
+00000015      LOCAL 0x30      
+00000016      LOAD4
+00000017        ARG 0x8       
+00000018      LOCAL 0x34      
+00000019      LOAD4
+0000001A        ARG 0xC       
+0000001B      LOCAL 0x38      
+0000001C      LOAD4
+0000001D        ARG 0x10      
+0000001E      CONST 0xD1C      ; CG_Init()
+0000001F       CALL
+00000020        POP
+00000021      CONST 0x0       
+00000022      LEAVE 0x24      
+00000023      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000024       JUMP
+00000025      CONST 0xE36      ; CG_Shutdown()
+00000026       CALL
+00000027        POP
+00000028      CONST 0x0       
+00000029      LEAVE 0x24      
+0000002A      CONST 0x85       ; vmMain()+0x85 (00000085)
+0000002B       JUMP
+0000002C      LOCAL 0x18      
+0000002D      CONST 0xFDC      ; CG_ConsoleCommand()
+0000002E       CALL
+0000002F     STORE4
+00000030      LOCAL 0x18      
+00000031      LOAD4
+00000032      LEAVE 0x24      
+00000033      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000034       JUMP
+00000035      LOCAL 0x30      
+00000036      LOAD4
+00000037        ARG 0x8       
+00000038      LOCAL 0x34      
+00000039      LOAD4
+0000003A        ARG 0xC       
+0000003B      LOCAL 0x38      
+0000003C      LOAD4
+0000003D        ARG 0x10      
+0000003E      CONST 0x1002B    ; CG_DrawActiveFrame()
+0000003F       CALL
+00000040        POP
+00000041      CONST 0x0       
+00000042      LEAVE 0x24      
+00000043      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000044       JUMP
+00000045      LOCAL 0x1C      
+00000046      CONST 0x17D      ; CG_CrosshairPlayer()
+00000047       CALL
+00000048     STORE4
+00000049      LOCAL 0x1C      
+0000004A      LOAD4
+0000004B      LEAVE 0x24      
+0000004C      CONST 0x85       ; vmMain()+0x85 (00000085)
+0000004D       JUMP
+0000004E      LOCAL 0x20      
+0000004F      CONST 0x18E      ; CG_LastAttacker()
+00000050       CALL
+00000051     STORE4
+00000052      LOCAL 0x20      
+00000053      LOAD4
+00000054      LEAVE 0x24      
+00000055      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000056       JUMP
+00000057      LOCAL 0x30      
+00000058      LOAD4
+00000059        ARG 0x8       
+0000005A      LOCAL 0x34      
+0000005B      LOAD4
+0000005C        ARG 0xC       
+0000005D      CONST 0xE3C      ; CG_KeyEvent()
+0000005E       CALL
+0000005F        POP
+00000060      CONST 0x0       
+00000061      LEAVE 0x24      
+00000062      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000063       JUMP
+00000064      LOCAL 0x30      
+00000065      LOAD4
+00000066        ARG 0x8       
+00000067      LOCAL 0x34      
+00000068      LOAD4
+00000069        ARG 0xC       
+0000006A      CONST 0xE3F      ; CG_MouseEvent()
+0000006B       CALL
+0000006C        POP
+0000006D      CONST 0x0       
+0000006E      LEAVE 0x24      
+0000006F      CONST 0x85       ; vmMain()+0x85 (00000085)
+00000070       JUMP
+00000071      LOCAL 0x30      
+00000072      LOAD4
+00000073        ARG 0x8       
+00000074      CONST 0xE39      ; CG_EventHandling()
+00000075       CALL
+00000076        POP
+00000077      CONST 0x0       
+00000078      LEAVE 0x24      
+00000079      CONST 0x85       ; vmMain()+0x85 (00000085)
+0000007A       JUMP
+0000007B      CONST 0x3DE9    
+0000007C        ARG 0x8       
+0000007D      LOCAL 0x2C      
+0000007E      LOAD4
+0000007F        ARG 0xC       
+00000080      CONST 0x1B8      ; CG_Error()
+00000081       CALL
+00000082        POP
+00000083      CONST 0xFFFFFFFF
+00000084      LEAVE 0x24      
+00000085       PUSH
+00000086      LEAVE 0x24      
 
-CG_RegisterCvars:
-    ENTER 1060
+CG_RegisterCvars():
+00000087      ENTER 0x424     
     ...
 ```
--- q3disasm.zig
+++ q3disasm.zig
@@ -6,7 +6,7 @@ const usage =
     \\Disassembles a QVM file to stdout.
     \\
     \\Options:
-    \\    -map <file>    Path to a .map file containing symbol info. If unspecified it'll look for one with the same name and directory as the input QVM file.
+    \\    -map <file>    Path to a .map file containing symbol info. If unspecified it'll try to find one with the same name and directory as the input QVM file.
     \\    -no-syms       Don't print symbol info with disassembly.
     \\    -header        Print the QVM header before disassembly.
     \\    -h|help        Print this help text and exit.
@@ -21,7 +21,9 @@ pub fn main() !void {
     var args = try std.process.argsWithAllocator(arena);
     _ = args.skip();
 
-    const stdout = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    const stdout = bw.writer();
+    defer bw.flush() catch |err| std.log.err("unable to flush to stdout: {s}", .{@errorName(err)});
 
     var qvm_path: ?[]const u8 = null;
     var map_path: ?[]const u8 = null;
@@ -114,44 +116,119 @@ pub fn main() !void {
 
     const code_seg = qvm[@intCast(header.code_offset)..][0..@intCast(header.code_length)];
 
-    var ip: usize = 0; // index into the code segment
-    var instruction: usize = 0; // counts which instruction we're on
+    var ip: u32 = 0; // index into the code segment
+    var instruction: u32 = 0; // counts which instruction we're on
+    var current_proc: ?*const Symbol = null; // track which procedure we're on (Unused when symbols == null)
+    defer std.debug.assert((symbols == null) == (current_proc == null));
 
     while (ip < code_seg.len) : (instruction += 1) {
-        if (symbols) |_symbols| for (_symbols) |sym| {
+        if (symbols) |_symbols| for (_symbols) |*sym| {
             if (sym.segment != .code) break; // map entries are sorted by segment and the code segment is always first.
             if (sym.offset == instruction) {
-                try stdout.print("\n{s}:\n", .{sym.name});
+                current_proc = sym;
+                try stdout.print("\n{s}():\n", .{sym.name});
             }
         };
 
+        try stdout.print("{X:0>8}", .{instruction});
+
         const op: Opcode = @enumFromInt(code_seg[ip]);
-        try stdout.print("    {s}", .{@tagName(op)});
+        try stdout.print(" {s: >10}", .{@tagName(op)});
         ip += 1;
 
         const arg_size = op.argumentSize();
         if (arg_size > 0) {
-            // const arg_bytes = code_seg[ip..][0..arg_size];
-            try stdout.print(" {d}", .{switch (arg_size) {
+            const arg: i32 = switch (arg_size) {
                 1 => code_seg[ip],
-                4 => std.mem.bytesToValue(i32, code_seg[ip..][0..4]),
+                // multi-byte arguments are little endian
+                4 => std.mem.littleToNative(i32, std.mem.bytesToValue(i32, code_seg[ip..][0..4])),
                 else => unreachable,
-            }});
+            };
             ip += arg_size;
+            try stdout.print(" 0x{X: <8}", .{@as(u32, @bitCast(arg))});
+
+            // print extra information about the argument if symbols are available
+            if (symbols) |_symbols| switch (op) {
+                .CONST => {
+                    switch (@as(Opcode, @enumFromInt(code_seg[ip]))) {
+                        .CALL => {
+                            for (_symbols) |sym| {
+                                if (sym.segment == .code and sym.offset == @as(u32, @bitCast(arg))) {
+                                    try stdout.print(" ; {s}()", .{sym.name});
+                                }
+                            }
+                        },
+                        .LOAD1, .LOAD2, .LOAD4 => {
+                            for (_symbols, 0..) |sym, i| {
+                                // LIT and BSS symbol offsets are encoded relative to the DATA segment in the instruction stream.
+                                // However, the .map file encodes them relative to their respective segments, so we need to apply
+                                // offsets before comparing to the LOAD address argument.
+                                const seg_offset: u32 = switch (sym.segment) {
+                                    .code => continue,
+                                    .data => 0,
+                                    .lit => @intCast(header.data_length),
+                                    .bss => @intCast(header.data_length + header.lit_length),
+                                };
+                                const sym_offset = sym.offset + seg_offset;
+
+                                const arg_u: u32 = @bitCast(arg);
+                                if (sym_offset == arg_u) {
+                                    try stdout.print(" ; {s}", .{sym.name});
+                                    break;
+                                }
+
+                                // check if arg is an offset into the symbol
+                                if (sym_offset < arg_u) {
+                                    // check the next symbol in the segment.
+                                    if (i + 1 < _symbols.len) {
+                                        const next = _symbols[i + 1];
+                                        if (next.segment != sym.segment) continue;
+                                        if (next.offset <= sym.offset) continue; // q3asm.c:HackToSegment() can cause symbol offsets to become non-contiguous
+                                        if (arg_u >= next.offset + seg_offset) continue;
+                                    } else continue; // if there isn't one we assume arg's not an offset
+
+                                    try stdout.print(" ; {s}+0x{X}", .{ sym.name, arg_u - sym_offset });
+                                    break;
+                                }
+                            }
+                        },
+                        .JUMP => try printJumpInfoComment(stdout, @bitCast(arg), current_proc.?),
+                        else => {},
+                    }
+                },
+                .EQ,
+                .NE,
+                .LTI,
+                .LEI,
+                .GTI,
+                .GEI,
+                .LTU,
+                .LEU,
+                .GTU,
+                .GEU,
+                .EQF,
+                .NEF,
+                .LTF,
+                .LEF,
+                .GTF,
+                .GEF,
+                => try printJumpInfoComment(stdout, @bitCast(arg), current_proc.?),
+                else => {},
+            };
         }
 
-        try stdout.print("\n", .{});
+        try stdout.writeByte('\n');
     }
 }
 
 const Segment = enum(u8) {
-    code,
+    code = 0,
     /// initialized 32 bit data, will be byte swapped
-    data,
+    data = 1,
     /// strings
-    lit,
+    lit = 2,
     /// 0 filled
-    bss,
+    bss = 3,
 };
 
 const Symbol = struct {
@@ -248,7 +325,9 @@ const Opcode = enum(u8) {
     PUSH,
     POP,
 
+    /// Followed by an i32 constant
     CONST,
+    /// Followed by an i32 offset from the stack register
     LOCAL,
 
     JUMP,
@@ -322,6 +401,9 @@ const Opcode = enum(u8) {
     CVIF,
     CVFI,
 
+    /// Returns the size of the opcode's argument in bytes.
+    /// Returns 0 if the opcode has no argument.
+    /// Multi-byte arguments are encoded as little endian.
     fn argumentSize(op: Opcode) u8 {
         return switch (op) {
             .ENTER,
@@ -329,7 +411,6 @@ const Opcode = enum(u8) {
             .LOCAL,
             .LEAVE,
 
-            // these opcodes have indirect arguments (value = [arg])
             .EQ,
             .NE,
             .LTI,
@@ -355,3 +436,9 @@ const Opcode = enum(u8) {
         };
     }
 };
+
+fn printJumpInfoComment(writer: anytype, jmp_target_instr: u32, procedure: *const Symbol) !void {
+    // based off instruction count, not byte offset!
+    const proc_rel_jmp_offset = jmp_target_instr - procedure.offset;
+    try writer.print(" ; {s}()+0x{X} ({X:0>8})", .{ procedure.name, proc_rel_jmp_offset, jmp_target_instr });
+}
